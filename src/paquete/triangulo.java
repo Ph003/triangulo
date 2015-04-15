d7534a23add731735ce079a547ac60614dfa43cd
EmaSuriano <emanuel.suriano@gmail.com>
2015-04-14 21:03:01 -0300

    esteban pt

--- src/paquete/triangulo.java
+++ src/paquete/triangulo.java
@@ -12,7 +12,7 @@ public class triangulo {
 	      BufferedReader br = null;
 	      int flag=0;
 	      
-
+	      	System.out.println("esteban pt");
 
 	      try {
 	         archivo = new File (path);
